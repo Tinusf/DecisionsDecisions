25d427f9efcac2b3df57dbdf8b3e4712fcf58282
Tinus Flagstad <tinus@flagstad.as>
2017-10-21 18:56:59 +0200

    added lots of studd like money, intelligence and happiness and the stats will now update on newDay()

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 //TODO: lag en date som øker med en dag hver gang progress bar er 100%
 const allPersons = document.getElementById("allPersons");
 let options = ["Work", "Education", "Gamble", "Socialise"]
+let persons = [];
 
 let date = new Date();
 showDate();
 startTimer();
 
 function addPerson() {
-	let person = new Person("tinus");
+	const personInfoChangable = ["Money", "Intelligence", "Happiness", "Age"];
+	const chosenName = prompt("Please enter your name", "Tinus"); //TODO: si hva slags kjønn det ble og endre farge ved forskjellig kjønn. ha en "other" kjønn?
+	let person = new Person(chosenName);
+
 	const div = document.createElement("div");
 	div.className = "person col-xs-3";
 
+	person.div = div;
+
 	const h4 = document.createElement("h4");
 	h4.innerHTML = person.name;
+	div.appendChild(h4);
+
+	const genderP = document.createElement("p");
+	genderP.innerHTML = "Gender: " + person.gender;
+	div.appendChild(genderP);
+
+	const birthdayP = document.createElement("p");
+	birthdayP.innerHTML = "Birthday: " + date.toLocaleDateString("nb-NO"); 
+	div.appendChild(birthdayP);
+	person.birthday = new Date(date.valueOf());
+
+
+	for (let i = 0; i < personInfoChangable.length; i++) {
+		const p = document.createElement("p");
+		p.innerHTML = personInfoChangable[i] + ": ";
+		div.appendChild(p);
+	}
+
 
 	const select = document.createElement("select");
 	select.className = "form-control";
@@ -23,25 +47,45 @@ function addPerson() {
 		select.appendChild(option);
 	}
 
-	div.appendChild(h4);
+	
 	div.appendChild(select);
 	allPersons.appendChild(div);
+	persons.push(person);
+	updateValueForPerson(person);
+}
+
+function updateValueForPerson(person) {
+	const childNodes = person.div.childNodes;
+	childNodes[3].innerHTML = "Money: " + person.money;
+	childNodes[4].innerHTML = "Intelligence: " + person.intelligence;
+	childNodes[5].innerHTML = "Happiness: " + person.happiness;
+	childNodes[6].innerHTML = "Age: " + person.getAge;
 }
 
 function updateValues() {
-	
+	persons.forEach(function(element) {
+		updateValueForPerson(element);
+	});
 }
 
 class Person {
 	constructor(name) {
 		//TODO lag startverdier random;
 		this.name = name;
-		this.age = 0;
 		this.money = 0;
 		this.intelligence = 0;
 		this.happiness = 0;
 		this.gender = "male";
-		this.birthday = "21.10.2017" //få tak i dagen i dag.
+		this.birthday;
+		this.div;
+	}
+	get getAge() {
+		console.log(date);
+		console.log(this.birthday);
+		let nowYear = date.getFullYear();
+		let pastYear = this.birthday.getFullYear();
+		let age = nowYear - pastYear;
+		return age;
 	}
 }
 
