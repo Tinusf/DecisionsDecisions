556f6bd414847979ce3f3d8d6733cf3b5534a942
Tinus Flagstad <tinus@flagstad.as>
2017-10-22 01:46:04 +0200

    added education, work and gambling.

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ checkStorage();
 appendTextConsole("Welcome to Decisions Decisions.");
 loadProgress();
 showDate();
-updateValues();
 
 function addPerson() {
 	const chosenName = prompt("Please enter your name", "Tinus"); //TODO: si hva slags kjønn det ble og endre farge ved forskjellig kjønn. ha en "other" kjønn?
@@ -93,7 +92,7 @@ function makePersonDiv(person) {
 	div.appendChild(buttonMakeKids);
 	
 	allPersons.appendChild(div);
-	updateValueForPerson(person);
+	updateValueForPerson(person, 0);
 	appendTextConsole("You have added a new person: " + person.name);
 }
 
@@ -118,35 +117,164 @@ function makeKids(person) {
 }
 
 function killSelf(person) {
+	appendTextConsole("This is incredibly sad, " + person.name + " just took " + hisOrHer(person) + " life.");
 	i = persons.indexOf(person);
 	delete persons[i];
 	person.div.remove();
 }
 
-function updateValueForPerson(person) {
+function isAdult(person) {
+	return calculateAge(person) >= 18;
+}
+
+function updateValueForPerson(person, daysChanged) {
 	const childNodes = person.div.childNodes;
-	
-	/*const moneyText = document.createTextNode("Money: " + person.money);
-	childNodes[3] = moneyText;
+	const activity = childNodes[7].value;
+	const adult = isAdult(person);
+	if (daysChanged > 0) {
+		switch(activity) {
+			case "Education": {
+				calculateEdu(person, daysChanged);
+				break;
+			}
+			case "Work": {
+				calculateWork(person, daysChanged);
+				break;
+			}
+			case "Gamble": {
+				calculateGamble(person, daysChanged);
+				break;
+			}
+			case "Socialise": {
+				calculateSocialise(person, daysChanged);
+				break;
+			}
+			default:
+				break;
+		}
+	}
 
-	const intText = document.createTextNode("Intelligence: " + person.intelligence);
-	childNodes[4] = intText;
-
-	const happyText = document.createTextNode("Happiness: " + person.happiness);
-	childNodes[5] = happyText;
-
-	const ageText = document.createTextNode("Age: " + person.getAge);
-	childNodes[6] = ageText;*/ //TODO: fiks noe lignende slik og ikke bruk innerHTML.
-
-	childNodes[3].innerHTML = "Money: " + person.money;
+	childNodes[3].innerHTML = "Money: " + person.money + "$";
 	childNodes[4].innerHTML = "Intelligence: " + person.intelligence;
 	childNodes[5].innerHTML = "Happiness: " + person.happiness;
 	childNodes[6].innerHTML = "Age: " + calculateAge(person);
 }
 
-function updateValues() {
+function heOrShe(person) {
+	let wordToUse;
+	if (person.gender == "male") {
+		wordToUse = "he";
+	} else {
+		wordToUse = "she";
+	}
+	return wordToUse;
+}
+
+function hisOrHer(person) {
+	let wordToUse;
+	if (person.gender == "male") {
+		wordToUse = "his";
+	} else {
+		wordToUse = "her";
+	}
+	return wordToUse;
+}
+
+function calculateEdu(person, daysChanged) {
+	const age = calculateAge(person);
+	let randomNumber = 0;
+	let outputString = "";
+	intelligenceRnd = Math.floor(Math.random() * 2);
+	happinessRnd = Math.floor(Math.random() * 2);
+	if (age < 6) {
+		outputString = "Little " + person.name + " went to kindergarden";
+	} else if (age < 13) {
+		outputString ="The young " + person.name + " went to elementary school";
+	} else if (age < 16) {
+		outputString = "The teen " + person.name + " went to middle school";
+	} else if (age < 19) {
+		outputString = "The young adult " + person.name + " went to high school";
+	} else {
+		outputString = "The wise " + person.name + " went to university";
+		moneyRnd = Math.floor(Math.random() * 2);
+		if (moneyRnd > 0) {
+			outputString += ", lost some money";
+			person.money -= moneyRnd;
+		}
+	}
+
+	if (intelligenceRnd > 0) {
+		outputString += " and gained some intelligence";
+		person.intelligence += daysChanged * intelligenceRnd;
+	}
+	if (happinessRnd > 0) {
+		outputString += " and had fun";
+		person.happiness += daysChanged * happinessRnd;
+	}
+	outputString += ".";
+	appendTextConsole(outputString);
+}
+
+function calculateWork(person, daysChanged) {
+	if (isAdult(person)) {
+		let intelligenceRnd = 0;
+		let happinessRnd = 0;
+		let moneyRnd = 0;
+		const wordToUse = heOrShe(person);
+		if (person.intelligence < 3500) {
+			appendTextConsole(person.name + " goes to work at McDonald " + wordToUse + " feels that " + wordToUse + " is getting dumber every day.");
+			intelligenceRnd = -Math.floor(Math.random() * 2);
+			moneyRnd = Math.floor(Math.random() * 2) +1;
+			happinessRnd = -Math.floor(Math.random() * 2) + 1;
+		} else if (person.intelligence < 5000) {
+			appendTextConsole(person.name + " goes to work as a life guard, it is alright.");
+			moneyRnd = Math.floor(Math.random() * 2) +1;
+		} else if (person.intelligence < 6000) {
+			appendTextConsole(person.name + " goes to work as a programmer, " + wordToUse + " loves it.");
+			moneyRnd = Math.floor(Math.random() * 5) +1;
+			happinessRnd = Math.floor(Math.random() * 2) + 1;
+		}
+		person.intelligence += intelligenceRnd * daysChanged;
+		person.money += moneyRnd * daysChanged;
+		person.happiness += happinessRnd * daysChanged;
+	} else {
+		appendTextConsole(person.name + " tried to get a job, but noone wants to hire him.");
+	}
+}
+
+function calculateGamble(person, daysChanged) {
+	if (isAdult(person)) {
+		const wordToUse = heOrShe(person);
+		const randomNumber = Math.floor(Math.random() * 30);
+		let moneyLostOrGained = 0;
+		if (person.money < 10) {
+			appendTextConsole(person.name + " just got kicked out of the casino " + wordToUse + " doesn't even have 10$, " + wordToUse + " is now sad.");
+			person.happiness -= 10 * daysChanged;
+		}
+		else if (randomNumber == 0) {
+			appendTextConsole("I can't believe it, " + person.name + " actually won money at the casino.");
+			person.happiness += 200 * daysChanged;
+			moneyLostOrGained = 200;
+		} else {
+			appendTextConsole(person.name + " tried to gamble and expectedly lost.");
+			moneyLostOrGained = -10;
+			person.happiness -= 1 * daysChanged;
+		}
+		person.money += moneyLostOrGained * daysChanged;
+	} else {
+		const finedAmount = -Math.floor(Math.random() * 100) + 1;
+		appendTextConsole(person.name + " tried to get into a casino whilst underage and got fined" + finedAmount + "$");
+		person.money += finedAmount * daysChanged;
+	}
+}
+
+function calculateSocialise(person, daysChanged) {
+
+}
+
+function updateValues(daysChanged) {
 	persons.forEach(function(element) {
-		updateValueForPerson(element);
+		updateValueForPerson(element, daysChanged);
 	});
 }
 
@@ -178,7 +306,7 @@ function showDate() {
 function nextDay() {
 	date.setDate(date.getDate() + 1);
 	showDate();
-	updateValues();
+	updateValues(1);
 	saveProgress();
 }
 
@@ -205,7 +333,7 @@ function appendTextConsole(text) {
 function hax() {
 	date.setDate(date.getDate() + 365);
 	showDate();
-	updateValues();
+	updateValues(365);
 }
 
 function checkStorage() {
@@ -232,6 +360,7 @@ function loadProgress() {
 	let loadedPersons = localStorage.getItem("persons");
 	if (loadedPersons != null) { // Hvis du aldri har lagret noe.
 		loadedPersons = loadedPersons.replace(new RegExp(",null", 'g'), "");
+		loadedPersons = loadedPersons.replace(new RegExp("null,", 'g'), "");
 		appendTextConsole("Loaded persons from file.");
 		if (loadedPersons != "[null]") { // Hvis du noen gang har lagret en null.
 			persons = JSON.parse(loadedPersons);
